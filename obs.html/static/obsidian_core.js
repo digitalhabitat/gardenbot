47a83b9f33c9be5a4dd2c5cab7ff4e3868f2ae11
digitalhabitat <miketm89@gmail.com>
2024-07-21 06:26:07 +0000

    Build: ()

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -13,7 +13,7 @@ var CONFIG_CLOSE_LEFT_PANE_IF_EMPTY = 0;
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '144884921268752411764310657228920520417'                       // used to check whether the localStorage data is stale
+var gzip_hash = '106964044820977570242833758093117965290'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
